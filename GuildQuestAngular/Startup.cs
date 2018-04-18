231c1dac8b7b11d4cf65f14ba8da8357f51990d7
Richard Turnmire <richardturnmire@outlook.com>
2018-04-18 03:36:30 -0400

    Initial commit with workng ng-bootstrap and csp settings

--- GuildQuestAngular/Startup.cs
+++ GuildQuestAngular/Startup.cs
@@ -1,17 +1,47 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Azure.KeyVault.Models;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using GuildQuestAngular.Utils;
 
 namespace GuildQuestAngular
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration config, IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+
+            if (env.IsDevelopment())
+            {
+                builder
+                    .AddUserSecrets<EmailConfiguration>()
+                    .AddJsonFile("appsettings.development.json", optional: false);
+            }
+
+            builder.AddEnvironmentVariables();
+
+            if (!env.IsDevelopment())
+            {
+                builder
+                    .AddAzureKeyVault(
+                        $"https://{config["vault"]}vault.vault.azure.net/",
+                        config["ClientId"],
+                        config["ClientSecret"],
+                        new PrefixKeyVaultSecretManager(""))
+                    .AddJsonFile("appsettings.json", optional: false);
+
+            }
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -20,12 +50,21 @@ namespace GuildQuestAngular
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
+            //services.AddHsts(options =>
+            //{
+            //    options.MaxAge = TimeSpan.FromDays(100);
+            //    options.IncludeSubDomains = true;
+            //    options.Preload = true;
+            //});
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+            services.AddCors(options => options.AddPolicy("MyCors", policy => policy.AllowAnyOrigin()));
+            services.AddSingleton<IEmailConfiguration>(Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>());
+            services.AddTransient<IEmailService, EmailService>();
+            services.AddSingleton<IConfiguration>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,13 +77,34 @@ namespace GuildQuestAngular
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
+               
             }
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add(
+                    "Content-Security-Policy",
+                    "frame-ancestors 'self' https:   ; " +
+                    "default-src 'self' https:   azure.net ; " +
+                    "child-src 'self' https:  ; " +
+                    "style-src 'self' blob: 'unsafe-inline' ; " +
+                    "font-src 'self' https: data: ; " +
+                    "script-src 'self' https: 'unsafe-eval' ; " +
+                    "connect-src 'self' https: wss: ; " +
+                    "img-src 'self' https: data: ; ");
 
+                context.Response.Headers.Add(
+                    "Strict-Transport-Security",
+                    "max-age=86400; includeSubDomains");
+
+                context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+                context.Response.Headers.Add("X-Xss-Protection", "1");
+                await next();
+            });
+            app.UseHsts();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
-
+            app.UseCors("MyCors");
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
@@ -58,12 +118,39 @@ namespace GuildQuestAngular
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
                 spa.Options.SourcePath = "ClientApp";
-
+               
                 if (env.IsDevelopment())
                 {
                     spa.UseAngularCliServer(npmScript: "start");
                 }
             });
+        }
+        public class PrefixKeyVaultSecretManager : IKeyVaultSecretManager
+        {
+            private readonly string _prefix;
+
+            public PrefixKeyVaultSecretManager(string prefix)
+            {
+                _prefix = $"{prefix}";
+            }
+
+            public bool Load(SecretItem secret)
+            {
+                // Load a vault secret when its secret name starts with the 
+                // prefix. Other secrets won't be loaded.
+                return secret.Identifier.Name.StartsWith(_prefix);
+            }
+
+            public string GetKey(SecretBundle secret)
+            {
+                // Remove the prefix from the secret name and replace two 
+                // dashes in any name with the KeyDelimiter, which is the 
+                // delimiter used in configuration (usually a colon). Azure 
+                // Key Vault doesn't allow a colon in secret names.
+                return secret.SecretIdentifier.Name
+                    .Substring(_prefix.Length)
+                    .Replace("--", ConfigurationPath.KeyDelimiter);
+            }
         }
     }
 }
